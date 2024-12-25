ad848d5b7a1e417a8959e8f1164f5352fd5034cf
Stephan Seitz <sseitz@nvidia.com>
2024-12-25 17:41:18 +0100

    Add luacheckrc/stylua

--- lua/nvim-dap-commands.lua
+++ lua/nvim-dap-commands.lua
@@ -16,71 +16,71 @@ end
 
 local python_adapter
 local python_path = select_executable { --[["uv",]]
-  "python3",
-  "python",
+  'python3',
+  'python',
 }
 
 -- from nvim-dap-python
-if python_path == "uv" then
+if python_path == 'uv' then
   python_adapter = {
-    type = "executable",
-    command = "uv",
-    args = { "run", "--with", "debugpy", "python", "-m", "debugpy.adapter" },
+    type = 'executable',
+    command = 'uv',
+    args = { 'run', '--with', 'debugpy', 'python', '-m', 'debugpy.adapter' },
     --enrich_config = enrich_config,
     options = {
-      source_filetype = "python",
+      source_filetype = 'python',
     },
   }
 else
   python_adapter = {
-    type = "executable",
+    type = 'executable',
     command = python_path,
-    args = { "-m", "debugpy.adapter" },
+    args = { '-m', 'debugpy.adapter' },
     --enrich_config = enrich_config,
     options = {
-      source_filetype = "python",
+      source_filetype = 'python',
     },
   }
 end
 
 local ENV = {}
 for k, v in pairs(vim.fn.environ()) do
-  table.insert(ENV, string.format("%s=%s", k, v))
+  table.insert(ENV, string.format('%s=%s', k, v))
 end
 
 local M = {}
 local defaults = {
   post_launch_commands = function()
-    local dap = require "dap"
+    local dap = require 'dap'
     dap.repl.open()
   end,
   debuggers = {
     gdb = {
-      cmd = "DebugGdb",
+      cmd = 'DebugGdb',
       adapter = {
-        type = "executable",
+        type = 'executable',
         attach = {
-          pidProperty = "pid",
-          pidSelect = "ask",
+          pidProperty = 'pid',
+          pidSelect = 'ask',
         },
         command = select_executable {
-          "rust-gdb",
-          "gdb",
+          'rust-gdb',
+          'gdb',
         },
         args = {
-          "--quiet",
-          "-i",
-          "dap",
+          '--quiet',
+          '-i',
+          'dap',
         },
-        name = "gdb",
+        name = 'gdb',
         stopOnEntry = false,
         --initCommands = 'command script import "/home/stephan/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/etc/lldb_lookup.py"',
       },
       config = function(args)
         return {
-          type = "rust",
+          type = 'rust',
           name = args[1],
-          request = "launch",
+          request = 'launch',
           program = table.remove(args, 1),
           args = args,
           cwd = vim.fn.getcwd(),
@@ -89,32 +89,32 @@ local defaults = {
       end,
     },
     lldb = {
-      cmd = "DebugLldb",
+      cmd = 'DebugLldb',
       adapter = {
-        type = "executable",
+        type = 'executable',
         attach = {
-          pidProperty = "pid",
-          pidSelect = "ask",
+          pidProperty = 'pid',
+          pidSelect = 'ask',
         },
         command = select_executable {
-          "lldb-dap-20",
-          "lldb-dap-19",
-          "lldb-vscode-18",
-          "lldb-vscode-17",
-          "lldb-vscode-16",
-          "lldb-vscode-15",
-          "lldb-vscode",
+          'lldb-dap-20',
+          'lldb-dap-19',
+          'lldb-vscode-18',
+          'lldb-vscode-17',
+          'lldb-vscode-16',
+          'lldb-vscode-15',
+          'lldb-vscode',
         },
         env = function()
           local variables = {
-            LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
+            LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = 'YES',
           }
           for k, v in pairs(vim.fn.environ()) do
-            table.insert(variables, string.format("%s=%s", k, v))
+            table.insert(variables, string.format('%s=%s', k, v))
           end
           return variables
         end,
-        name = "lldb",
+        name = 'lldb',
         stopOnEntry = false,
         --initCommands = 'command script import "/home/stephan/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/etc/lldb_lookup.py"',
       },
@@ -124,9 +124,9 @@ local defaults = {
         end, args)
         if args and #args > 0 then
           return {
-            type = "rust",
+            type = 'rust',
             name = args[1],
-            request = "launch",
+            request = 'launch',
             program = table.remove(args, 1),
             env = ENV,
             args = args,
@@ -134,22 +134,22 @@ local defaults = {
             environment = {},
             stopOnEntry = false,
             externalConsole = true,
-            expressions = "python",
+            expressions = 'python',
             --initCommands = get_init_commands(),
           }
         end
       end,
     },
     debugpy = {
-      cmd = "Debugpy",
+      cmd = 'Debugpy',
       adapter = python_adapter,
       config = function(args)
         return {
-          type = "python",
+          type = 'python',
           name = args[1],
-          console = "integratedTerminal",
+          console = 'integratedTerminal',
           justMyCode = true,
-          request = "launch",
+          request = 'launch',
           program = table.remove(args, 1),
           args = args,
           --pythonPath = function()
@@ -162,16 +162,16 @@ local defaults = {
 }
 
 function M.setup(opts)
-  opts = vim.tbl_deep_extend("keep", opts, defaults)
+  opts = vim.tbl_deep_extend('keep', opts, defaults)
   for k, v in pairs(opts.debuggers) do
     if v.cmd then
       vim.api.nvim_create_user_command(v.cmd, function(t)
-        local dap = require "dap"
+        local dap = require 'dap'
         local config
         if #t.fargs == 0 then
           if not v.last_config then
             vim.notify(
-              "Please provide arguments to " .. v.cmd .. " (the program with arguments that you want to launch)",
+              'Please provide arguments to ' .. v.cmd .. ' (the program with arguments that you want to launch)',
               vim.log.levels.ERROR
             )
             return
@@ -180,8 +180,8 @@ function M.setup(opts)
         else
           config = v.config(t.fargs)
         end
-        if type(config) == "string" then
-          vim.notify("Could not launch debug adapter: " .. config, vim.log.levels.ERROR)
+        if type(config) == 'string' then
+          vim.notify('Could not launch debug adapter: ' .. config, vim.log.levels.ERROR)
           return
         end
         if config then
@@ -189,7 +189,7 @@ function M.setup(opts)
           v.last_config = config
         else
           vim.notify(
-            "Could not launch debug adapter " .. k .. ": user provided config function returned nil",
+            'Could not launch debug adapter ' .. k .. ': user provided config function returned nil',
             vim.log.levels.ERROR
           )
         end
@@ -197,8 +197,8 @@ function M.setup(opts)
           opts.post_launch_commands(t)
         end
       end, {
-        complete = "file",
-        nargs = "*",
+        complete = 'file',
+        nargs = '*',
       })
     end
   end
